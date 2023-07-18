c556ffae9f26c2c1d665f3438a06d7ba7f7119d4
Prathamesh <prathameshapsb@gmail.com>
2023-07-18 20:35:23 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Initialize PyGame
 pygame.init()
@@ -14,10 +15,22 @@ pygame.display.set_icon(icon)
 player_Img = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
+
+# Enemy
+Enemy_Img = pygame.image.load('enemy.png')
+EnemyX = random.randint(0,800)
+EnemyY = random.randint(50,150)
+EnemyX_change = 0.3
+EnemyY_change = 0
 
 
 def player(x, y):
     screen.blit(player_Img, (x, y))
+
+
+def Enemy(x, y):
+    screen.blit(Enemy_Img, (x, y))
 
 
 # Game Loop
@@ -30,15 +43,27 @@ while running:
         # If keystroke is pressed check whether it is right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                print("Left arrow key is pressed")
+                playerX_change = -0.3
             if event.key == pygame.K_RIGHT:
-                print("Right arrow key is pressed")
+                playerX_change = 0.3
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                print("Keystroke has been released")
-
+                playerX_change = 0
 
     # RGB
     screen.fill((0, 0, 0))
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    EnemyX += EnemyX_change
+    if EnemyX <= 0:
+        EnemyX_change = 0.3
+    elif EnemyX >= 736:
+        EnemyX_change = -0.3
+
     player(playerX, playerY)
+    Enemy(EnemyX,EnemyY)
     pygame.display.update()
